8d20c8f4b15618be29d76eb24ef42ae5c9b4842f
Nextia <rozakidwi@gmail.com>
2025-04-11 11:18:14 +0700

    nvim/init.lua: Change line number color to gray

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -43,7 +43,9 @@ require("lazy").setup({
 require('ayu').setup({
     mirage = true, -- Set to `true` to use `mirage` variant instead of `dark` for dark background.
     terminal = true, -- Set to `false` to let terminal manage its own colors.
-    overrides = {}, -- A dictionary of group names, each associated with a dictionary of parameters (`bg`, `fg`, `sp` and `style`) and colors in hex.
+    overrides = {
+        LineNr = { fg = "gray" }, 
+    }, -- A dictionary of group names, each associated with a dictionary of parameters (`bg`, `fg`, `sp` and `style`) and colors in hex.
 })
 
 vim.cmd("colorscheme ayu-mirage")
