a63b2fcd80869085a61fc32e266bf5505f9f38b3
NextiaIndex <nextiaindex@protonmail.com>
2025-06-16 18:17:04 +0700

    add lsp for python and awesome for lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -313,7 +313,28 @@ vim.keymap.set('n', '<leader>t', ':NvimTreeToggle<CR>')
 				})
 			end,
 			settings = {
-				Lua = {}
+				Lua = {
+					workspace = {
+						checkThirdParty = false,
+						library = vim.tbl_deep_extend(
+							'force',
+							vim.api.nvim_get_runtime_file("", true),
+							{
+								"/usr/share/awesome/lib"
+							}
+						)
+					},
+					diagnostics = {
+						globals = {
+							"awesome",
+							"awful",
+							"client",
+							"screen",
+							"tag",
+							"root"
+						}
+					}
+				}
 			}
 		}
 	lspconfig.html.setup {
@@ -337,5 +358,56 @@ vim.keymap.set('n', '<leader>t', ':NvimTreeToggle<CR>')
 			scss = { validate = true },
 			less = { validate = true },
 		},
+	}
+	local function set_python_path(path)
+		local clients = vim.lsp.get_clients {
+			bufnr = vim.api.nvim_get_current_buf(),
+			name = 'pyright',
+		}
+		for _, client in ipairs(clients) do
+			if client.settings then
+				client.settings.python = vim.tbl_deep_extend('force', client.settings.python, { pythonPath = path })
+			else
+				client.config.settings = vim.tbl_deep_extend('force', client.config.settings, { python = { pythonPath = path } })
+			end
+			client.notify('workspace/didChangeConfiguration', { settings = nil })
+		end
+	end
+	lspconfig.pyright.setup {
+		cmd = { 'pyright-langserver', '--stdio' },
+		filetypes = { 'python' },
+		root_markers = {
+			'pyproject.toml',
+			'setup.py',
+			'setup.cfg',
+			'requirements.txt',
+			'Pipfile',
+			'pyrightconfig.json',
+			'.git',
+		},
+		settings = {
+			python = {
+				analysis = {
+					autoSearchPaths = true,
+					useLibraryCodeForTypes = true,
+					diagnosticMode = 'openFilesOnly',
+				},
+			},
+		},
+		on_attach = function(client, bufnr)
+				vim.api.nvim_buf_create_user_command(bufnr, 'LspPyrightOrganizeImports', function()
+					client:exec_cmd({
+						command = 'pyright.organizeimports',
+						arguments = { vim.uri_from_bufnr(bufnr) },
+					})
+				end, {
+				desc = 'Organize Imports',
+			})
+			vim.api.nvim_buf_create_user_command(bufnr, 'LspPyrightSetPythonPath', set_python_path, {
+				desc = 'Reconfigure pyright with the provided python path',
+				nargs = 1,
+				complete = 'file',
+			})
+		end
 	}
 --
