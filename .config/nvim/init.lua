e6d4db432e531ece53f5f95fce06eaf73fa85d22
NextiaIndex <nextiaindex@protonmail.com>
2025-06-29 07:59:42 +0700

    nvim/init.lua: add more lsps and render-markdown.nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -215,7 +215,9 @@ require("lazy").setup({
 					"html",
 					"cssls",
 					"pyright",
-					"intelephense"
+					"intelephense",
+					"lemminx",
+					"bashls"
 				}
 			},
 			dependencies = {
@@ -242,6 +244,16 @@ require("lazy").setup({
 		{
 			"numToStr/Comment.nvim",
 			config = true
+		},
+
+		{
+			'MeanderingProgrammer/render-markdown.nvim',
+			dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
+			-- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+			-- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+			---@module 'render-markdown'
+			---@type render.md.UserConfig
+			opts = {},
 		}
 	},
 	install = { colorscheme = { "habamax" } },
