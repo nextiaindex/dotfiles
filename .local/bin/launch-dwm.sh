a7c726b5f8bd625bd99387be59f5b189b9c3e0ef
NextiaIndex <nextiaindex@protonmail.com>
2025-10-06 12:11:51 +0700

    Change feh  command to use vague.png as wallpaper.

--- .local/bin/launch-dwm.sh
+++ .local/bin/launch-dwm.sh
@@ -1,5 +1,5 @@
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
-feh --bg-fill ~/Wallpapers/ayumiragesky.jpg &
+feh --bg-fill ~/Wallpapers/vague.png &
 dwmblocks &
 picom &
 exec dbus-run-session dwm
