11628a79d23d5cf5709fe257b060adf3a7ab9ade
NextiaIndex <nextiaindex@protonmail.com>
2025-12-09 20:38:52 +0700

    Change wallpaper to jakarta1red.png.

--- .local/bin/launch-dwm.sh
+++ .local/bin/launch-dwm.sh
@@ -1,5 +1,5 @@
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
-feh --bg-fill ~/Wallpapers/vague.png &
+feh --bg-fill ~/Wallpapers/jakarta1red.png &
 dwmblocks &
 picom &
 exec dbus-run-session dwm
