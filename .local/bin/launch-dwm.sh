7f0150198f5247cf215adf0a27f9d28132f03bd5
NextiaIndex <nextiaindex@protonmail.com>
2025-12-15 21:22:58 +0700

    Change feh's wallpaper path to vague2.png
    From now, I'm using this wallpaper.

--- .local/bin/launch-dwm.sh
+++ .local/bin/launch-dwm.sh
@@ -1,5 +1,5 @@
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
-feh --bg-fill ~/Wallpapers/jakarta1red.png &
+feh --bg-fill ~/Wallpapers/vague2.png &
 dwmblocks &
 picom &
 exec dbus-run-session dwm
